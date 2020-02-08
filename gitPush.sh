542cf3b60c6edaf2241cf0b7897832a7e694ff03
karan <kgalgat117@gmail.com>
2020-02-09 02:46:59 +0530

    some more

---
+++ gitPush.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git add .
+git commit -m "some more"
+git push -u origin master
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { CoreModule } from './core/core.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CoreModule,
     BrowserModule,
     AppRoutingModule
   ],
