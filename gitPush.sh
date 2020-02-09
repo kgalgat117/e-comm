714887464cc39b7697f83acf9215bd80421bbcbf
karan <kgalgat117@gmail.com>
2020-02-09 18:53:57 +0530

    some

--- gitPush.sh
+++ gitPush.sh
@@ -1,11 +1,10 @@
-
 #!/bin/bash
 git add .
-git commit -m "some more"
+git commit -m "$1"
 git push -u origin master
 
-expect "username:"
+expect "Username for 'https://github.com':"
 send "$2"
 
-expect "password:"
+expect "Password for 'https://$2@github.com':"
 send "$3"
